0d97891d6e77966f04b359f4fb55deb3c936b9c1
David Zager <david.j.zager@gmail.com>
2019-07-26 15:00:03 -0400

    Some more documentation cleanup

--- conditions/v1/_doc.go
+++ conditions/v1/_doc.go
@@ -2,5 +2,8 @@
 // +k8s:defaulter-gen=TypeMeta
 // +k8s:openapi-gen=true
 
-// Package v1 is the v1 version of the API.
+// Package v1 provides version v1 of the types and functions necessary to
+// manage and inspect a slice of conditions. It is opinionated in the
+// condition types provided but leaves it to the user to define additional
+// types as necessary.
 package v1
